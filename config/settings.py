05523175e71fc3bc4c314b9c81dc1cc7bca88a80
SeoungGeunYeol <swswaq11@naver.com>
2022-05-11 18:57:47 +0900

    Postgres Part2

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com", ""]
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 # Application definition
 DJANGO_APPS = [
